7f27b0815aec1c45ae41d023a9b09a5f9e50503c
TwarochUNIT <63618753+TwarochUNIT@users.noreply.github.com>
2020-04-17 14:25:45 +0200

    main menu 2
    new main menu with smoke effect

---
+++ Assets/MainMenu/Main2/MovePlane.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlane : MonoBehaviour
+{
+    public float movementSpeed;
+    public Vector3 originalPos;
+    public Vector3 currentPos;
+    public GameObject plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.position;
+        InvokeRepeating("Reset", 4.0f, 4.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right* Time.deltaTime * movementSpeed;
+
+    }
+
+    void Reset()
+    {
+        currentPos = originalPos;
+        transform.position = originalPos;
+	}
+}
---
+++ Assets/MainMenu/Main2/ShipWobble.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipWobble : MonoBehaviour
+{
+    //adjust this to change speed
+public float speed = 0.5f;
+//adjust this to change how high it goes
+public float height = 0.2f;
+
+
+ 
+void Update() {
+       
+ }
+}
